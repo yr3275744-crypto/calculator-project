a8554e5c3b5b764644e526db23dda1bbf105960a
Ytshak_Raiyner <yr3275744@gmail.com>
2026-02-02 15:10:32 +0200

    add divide

--- calculator.py
+++ calculator.py
@@ -3,3 +3,13 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+
+# multiplay two numbers
+def multiply(a,b):
+    return a*b
+
+def dividing(a, b):
+    if b == 0:
+        print("divid by 0..")
+        return
+    return a / b
